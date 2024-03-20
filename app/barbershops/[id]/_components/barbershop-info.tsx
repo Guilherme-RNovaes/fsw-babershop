5d9622178c35acf6ec53bc9c98269a9b1d71f0d2
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-03-20 19:04:20 -0300

    feat: add SideMenu component

--- app/barbershops/[id]/_components/barbershop-info.tsx
+++ app/barbershops/[id]/_components/barbershop-info.tsx
@@ -1,6 +1,8 @@
 "use client"
 
+import SideMenu from "@/app/_components/side-menu";
 import { Button } from "@/app/_components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/app/_components/ui/sheet";
 import { Barbershop } from "@prisma/client";
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
 import Image from "next/image";
@@ -10,22 +12,29 @@ interface BarbershopInfoProps {
   barbershop: Barbershop;
 }
 
-const BarbershopInfo = ({barbershop}: BarbershopInfoProps) => {
+const BarbershopInfo = ({ barbershop }: BarbershopInfoProps) => {
   const router = useRouter()
 
   const handleBackClick = () => {
     router.back()
   }
-  return( 
+  return (
     <div>
       <div className="h-[250px] w-full relative">
         <Button onClick={handleBackClick} size='icon' variant='outline' className="z-50 absolute top-4 left-4">
           <ChevronLeftIcon />
         </Button>
-        <Button size='icon' variant='outline' className="z-50 absolute top-4 right-4">
-          <MenuIcon />
-        </Button>
-        <Image src={barbershop.imageUrl} fill alt={barbershop.name} style={{ objectFit: 'cover', }} className="opacity-75"/>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size='icon' variant='outline' className="z-50 absolute top-4 right-4">
+              <MenuIcon size={18} />
+            </Button>
+          </SheetTrigger>
+          <SheetContent className="p-0">
+            <SideMenu />
+          </SheetContent>
+        </Sheet>
+        <Image src={barbershop.imageUrl} fill alt={barbershop.name} style={{ objectFit: 'cover', }} className="opacity-75" />
       </div>
       <div className="px-5 pt-3 pb-6 border-b border-solid border-secondary">
         <h1 className="text-xl font-bold">{barbershop.name}</h1>
