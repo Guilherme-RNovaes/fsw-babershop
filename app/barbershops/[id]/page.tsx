59e35cc3ff7e9e8709d9df36b557306d65517177
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-03-20 08:30:38 -0300

    feat: add barbershop-info.tsx component

---
+++ app/barbershops/[id]/page.tsx
@@ -0,0 +1,33 @@
+import { Button } from "@/app/_components/ui/button";
+import { db } from "@/app/_lib/prisma";
+import { ChevronLeftIcon, Heading1, MapPinIcon, MenuIcon, StarIcon } from "lucide-react";
+import Image from "next/image";
+import BarbershopInfo from "./_components/barbershop-info";
+
+interface BarbershopDetailsPageProps {
+  params: {
+    id?: string;
+  };
+}
+const BarbershopDetailsPage = async ({params}: BarbershopDetailsPageProps) => {
+  if (!params.id) {
+    // TODO: redirecionar para home page
+    return null
+  }
+
+  const barbershop = await db.barbershop.findUnique({
+    where: {
+      id: params.id,
+    }
+  });
+
+  if (!barbershop) {
+    // TODO: redirecionar para home page
+    return null;
+  }
+  return ( 
+    <BarbershopInfo barbershop={barbershop}/>
+  );
+}
+
+export default BarbershopDetailsPage;
