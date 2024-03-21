4ce1a84a1fb14453577eaf0f14d785f502228407
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-03-21 20:17:34 -0300

    feat: add initial calendar

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -1,10 +1,14 @@
 "use client"
 
+import React from "react";
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
+import { Sheet, SheetContent, SheetHeader, SheetTitle, SheetTrigger } from "@/app/_components/ui/sheet";
 import { Service } from "@prisma/client";
 import { signIn } from "next-auth/react";
 import Image from "next/image";
+import { Calendar } from "@/app/_components/ui/calendar";
+import { ptBR } from "date-fns/locale";
 
 interface ServiceItemProps {
   service: Service;
@@ -12,6 +16,7 @@ interface ServiceItemProps {
 }
 
 const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
+  const [date, setDate] = React.useState<Date | undefined>(new Date())
   const handleBookingClick = () => {
     if (!isAuthenticated) {
       return signIn("google")
@@ -35,9 +40,45 @@ const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
                   currency: "BRL",
                 }).format(Number(service.price))}
               </p>
-              <Button variant='secondary' onClick={handleBookingClick}>
-                Reservar
-              </Button>
+              <Sheet>
+                <SheetTrigger asChild>
+                  <Button variant='secondary' onClick={handleBookingClick}>
+                    Reservar
+                  </Button>
+                </SheetTrigger>
+                <SheetContent className="p-0">
+                  <SheetHeader className="text-left px-5 py-6 border-b border-solid border-secondary">
+                    <SheetTitle>Fazer Reserva</SheetTitle>
+                  </SheetHeader>
+                  <Calendar mode="single" selected={date} onSelect={setDate} fromDate={new Date()} locale={ptBR}
+                    className="mt-6"
+                    styles={{
+                      head_cell: {
+                        width: '100%',
+                        textTransform: 'capitalize',
+                      },
+                      cell: {
+                        width: '100%',
+                      },
+                      button: {
+                        width: '100%',
+                      },
+                      nav_button_previous: {
+                        width: '32px',
+                        height: '32px',
+                      },
+                      nav_button_next: {
+                        width: '32px',
+                        height: '32px',
+                      },
+                      caption: {
+                        textTransform: 'capitalize',
+                      },
+                    }}
+                  />
+                  {/* mostrar lista de horários somente se alguma data estiver selecionada */}
+                </SheetContent>
+              </Sheet>
             </div>
           </div>
         </div>
