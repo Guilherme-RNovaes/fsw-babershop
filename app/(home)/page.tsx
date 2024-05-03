d3a7eb59b2dd73e0a3457b7ab25b11a3c80f077e
Guilherme-RNovaes <guilherme00rogerio@gmail.com>
2024-05-03 10:05:21 -0300

    feat: ensure confirmed bookings are shown in home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -9,19 +9,23 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 
 export default async function Home() {
-  const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
-  const barbershops = await db.barbershop.findMany({})
-
-  const bookings = session?.user ? await db.booking.findMany({
-    where: {
-      userId: (session.user as any).id,
-    },
-    include: {
-      service: true,
-      barbershop: true,
-    }
-  }) : [];
+  const [barbershops, confirmedBookings] = await Promise.all([
+    db.barbershop.findMany({}),
+    session?.user ? db.booking.findMany({
+      where: {
+        userId: (session.user as any).id,
+        date: {
+          gte: new Date(),
+        },
+      },
+      include: {
+        service: true,
+        barbershop: true,
+      },
+    }) : Promise.resolve([]),
+  ])
 
   return (
     <div>
@@ -43,7 +47,7 @@ export default async function Home() {
       <div className="mt-6">
         <h2 className=" pl-5 text-xs mb-3 uppercase text-gray-400 font-bold">Agendamentos</h2>
         <div className="px-5 flex gap-3 overflow-x-auto [&::-webkit-scrollbar]:hidden">
-          {bookings.map((booking: any) =>
+          {confirmedBookings.map((booking: any) =>
             <BookingItem key={booking.id} booking={booking} />
           )}
         </div>
